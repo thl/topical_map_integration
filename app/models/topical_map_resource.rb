80ae05d8ee2677b87c618caca434af12a411a58d
Andres Montano <amontano@virginia.edu>
2010-02-08 18:54:59 -0600

    Updating local path to kmaps app.

--- app/models/topical_map_resource.rb
+++ app/models/topical_map_resource.rb
@@ -7,7 +7,7 @@ class TopicalMapResource < ActiveResource::Base
   elsif hostname == 'apoc.village.virginia.edu'  
     self.site = 'http://shanti.virginia.edu/kmaps/'    
   elsif hostname.ends_with? 'local'
-    self.site = 'http://localhost/trunk/tmb/'
+    self.site = 'http://localhost/master/kmaps/'
   else
     self.site = 'http://tmb.thlib.org/' #'http://localhost/trunk/tmb/'
   end  

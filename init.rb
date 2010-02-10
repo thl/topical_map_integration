54bc22b26bb81bf86ab971913819af6a625a58a0
Andres Montano <amontano@virginia.edu>
2010-02-10 11:28:54 -0600

    Taking out all active resource patches.

--- init.rb
+++ init.rb
@@ -3,6 +3,5 @@ ActiveResource::Base.send :include, ActiveResource::Extensions::URL
 ActiveResource::Base.send :include, ActiveResource::Extensions::CustomURL
 
 # Include hook code here
-require 'patches/active_resource_patch'
 require 'topical_map_builder_integration'
 ActionView::Base.send :include, TopicalMapCategoriesHelper

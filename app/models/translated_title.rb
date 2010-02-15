e9412ec261b75a25869224cb6aa6fef5c8cf1f69
Andres Montano <amontano@virginia.edu>
2010-02-15 10:14:46 -0600

    Header in hash can't be nil. Must have a value or not be set at all.

--- app/models/translated_title.rb
+++ app/models/translated_title.rb
@@ -1,4 +1,4 @@
 class TranslatedTitle < TopicalMapResource
-  headers['Host'] = TopicalMapResource.headers['Host']
+  headers['Host'] = TopicalMapResource.headers['Host'] if !TopicalMapResource.headers['Host'].blank?
   self.site = "#{self.site.to_s}#{Category.collection_name}/:#{Category.element_name}_id/"
 end

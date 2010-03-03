622fe72d234e7364398a9482479a1c748db85f8a
Tom Benner <tbb4n@virginia.edu>
2010-03-02 20:03:41 -0500

    Adding a namespaced proxy script

---
+++ app/controllers/kmaps_integration/utils_controller.rb
@@ -0,0 +1,40 @@
+class KmapsIntegration::UtilsController < ApplicationController
+  
+  def proxy
+    
+    # We want to grab params that are part of the requested URL, but ignore ones that are supplied by Rails
+    ignored_params = ["proxy_url", "action", "controller"]
+    url_params = params.reject{|param, val| ignored_params.include?(param) }.collect{ |param, val| param + '=' + CGI.escape(val) }.join('&')
+    
+    url = params[:proxy_url]
+    url += '&' + url_params unless url_params.blank?
+    
+    # Parse the URL with URI.parse() so we can work with its parts more easily
+    uri = URI.parse(URI.encode(url));
+    requested_host = uri.host
+    headers = {}
+    
+    # Check to see if the request is for a URL on thlib.org or a subdomain; if so, and if
+    # this is being run on sds[3-8], make the appropriate changes to headers and uri.host
+    if requested_host =~ /thlib.org/
+      server_host = Socket.gethostname.downcase
+      if server_host =~ /sds[3-8].itc.virginia.edu/
+        headers = { 'Host' => requested_host }
+        uri.host = '127.0.0.1'
+      end
+    end
+    
+    # Required for requests without paths (e.g. http://www.google.com)
+    uri.path = "/" if uri.path.empty?
+    
+    path = uri.query.blank? ? uri.path : uri.path + '?' + uri.query 
+    request = Net::HTTP::Get.new(path, headers)
+    result = Net::HTTP.start(uri.host, uri.port) {|http|
+      http.request(request)
+    }
+    
+    render :text => result.body
+    
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ ActionController::Routing::Routes.draw do |map|
   map.resources(:categories, :member => {:expand => :get, :contract => :get}) do |category|
     category.resources(:children, :controller => 'categories', :member => {:expand => :get, :contract => :get})
   end
+  
+  map.namespace(:kmaps_integration) do |kmaps_integration|
+    kmaps_integration.connect 'utils/proxy/', :controller => 'utils', :action => 'proxy'
+  end
 end

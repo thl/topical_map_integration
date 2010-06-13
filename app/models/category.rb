121851e54027af6cf4e8de88efbed2bda4bbee93
Andres Montano <amontano@virginia.edu>
2010-06-12 22:28:19 -0600

    taking common functionality out and putting it in thl/active_resource_extensions

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,10 +2,9 @@ class Category < TopicalMapResource
   headers['Host'] = TopicalMapResource.headers['Host'] if !TopicalMapResource.headers['Host'].blank?
   
   acts_as_active_resource_tree
-  @@cached_categories_by_title = Hash.new
   
   def self.find_by_title(title)
     # Since its going as part of the url, space is not welcome on the rails side as a '+'. Should be %20 instead.
-    @@cached_categories_by_title[title] ||= find(:first, :from => "#{prefix}#{collection_name}/by_title/#{CGI::escape(title).gsub(/\+/, '%20')}.xml")
+    self.find(:first, :from => "#{prefix}#{collection_name}/by_title/#{CGI::escape(title).gsub(/\+/, '%20')}.xml")
   end
 end
--- init.rb
+++ init.rb
@@ -1,6 +1,3 @@
-ActiveResource::Base.send :include, ActiveResource::Acts::Tree
-ActiveResource::Base.send :include, ActiveResource::Extensions::URL
-
 # Include hook code here
 require 'topical_map_builder_integration'
 ActionView::Base.send :include, TopicalMapCategoriesHelper

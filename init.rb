7e39d35f364d498682476c485275d053d5a1bcf0
Andres Montano <amontano@virginia.edu>
2012-01-03 20:33:48 -0600

    adding translation of active resource models

--- init.rb
+++ init.rb
@@ -1,3 +1,5 @@
 # Include hook code here
+#I18n.load_path << File.join(File.dirname(__FILE__), 'config', 'locales')
+I18n.load_path += Dir[File.join(File.dirname(__FILE__), 'config', 'locales', '*.yml')]
 require 'topical_map_builder_integration'
 ActionView::Base.send :include, TopicalMapCategoriesHelper

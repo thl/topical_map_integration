292989c3f22e51e8b908a8357c8aba7cce6e1a3e
Andres Montano <amontano@virginia.edu>
2014-02-14 21:53:00 -0600

    code is not ruby 2.0 compliant (not backward-compatible)

--- lib/topical_map_integration.rb
+++ lib/topical_map_integration.rb
@@ -2,7 +2,7 @@ require 'topical_map_integration/engine'
 
 # Include hook code here
 #I18n.load_path << File.join(File.dirname(__FILE__), 'config', 'locales')
-I18n.load_path += Dir[File.join(File.dirname(__FILE__), '..', 'config', 'locales', '*.yml')]
+I18n.load_path += Dir[File.join(__dir__, '..', 'config', 'locales', '*.yml')]
 # ActionView::Base.send :include, TopicalMapCategoriesHelper
 
 module TopicalMapIntegration
